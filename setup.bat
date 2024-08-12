b5af9f4486123c73254a983ead9a78ed9204a3d8
KelSiken <oguzfirat14@gmail.com>
2024-08-12 03:22:08 +0300

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Installing Python libraries...
+
+REM Install Python packages
+pip install requests
+pip install colorama
+pip install selenium
+pip install tk
+
+echo All libraries have been installed.
+pause
